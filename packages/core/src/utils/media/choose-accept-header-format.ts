b7ee0fff18e189210376e9cc3daefc8ae5d3bcba
William Yallop <hello@williamyallop.com>
2024-11-20 20:43:43 +0000

    WebP prioritised over AVIF on stream media service

--- packages/core/src/utils/media/choose-accept-header-format.ts
+++ packages/core/src/utils/media/choose-accept-header-format.ts
@@ -5,8 +5,8 @@ const chooseAcceptHeaderFormat = (
 	if (queryFormat) return queryFormat;
 
 	if (accept) {
-		if (accept.includes("image/avif")) return "avif";
 		if (accept.includes("image/webp")) return "webp";
+		if (accept.includes("image/avif")) return "avif";
 	}
 
 	return undefined;
