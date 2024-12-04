1251c84e87ec14a3204be8565e4a98365c46569f
William Yallop <hello@williamyallop.com>
2024-12-04 20:42:25 +0000

    Defining CollectionSchema shape

--- packages/core/src/services/collection-migrator/schema/types.ts
+++ packages/core/src/services/collection-migrator/schema/types.ts
@@ -1,3 +1,37 @@
+export type TableType =
+	| "document"
+	| "versions"
+	| "fields"
+	| "brick"
+	| "repeater";
+
+export type DataType =
+	| "text"
+	| "int"
+	| "timestamp"
+	| "serial"
+	| "float"
+	| "varchar";
+
 export type CollectionSchema = {
 	key: string;
+	tables: Array<{
+		name: string;
+		type: TableType;
+		columns: Array<{
+			key: string;
+			source: "core" | "field";
+			dataType: DataType;
+			nullable: boolean;
+			defaultValue?: unknown;
+			foreignKey?: {
+				table: string;
+				column: string;
+				onDelete?: "CASCADE" | "SET NULL" | "RESTRICT";
+				onUpdate?: "CASCADE" | "SET NULL" | "RESTRICT";
+			};
+			unique?: boolean;
+			primary?: boolean;
+		}>;
+	}>;
 };
