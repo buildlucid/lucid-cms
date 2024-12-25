38bbdf2b51bbab625a7f17d699526daaff89dcfd
William Yallop <hello@williamyallop.com>
2024-12-25 12:22:24 +0000

    Started building out modify column and finished dropColumn

--- packages/core/src/services/collection-migrator/migration/column-builder.ts
+++ packages/core/src/services/collection-migrator/migration/column-builder.ts
@@ -8,17 +8,19 @@ import type {
 	CreateTableBuilder,
 	AlterTableColumnAlteringBuilder,
 	AlterTableBuilder,
+	AlteredColumnBuilder,
+	AlterColumnBuilder,
 } from "kysely";
-
-type QueryBuilder =
-	| CreateTableBuilder<string, never>
-	| AlterTableColumnAlteringBuilder
-	| AlterTableBuilder;
 
 /**
  * Adds a column to a table using the provided query builder
  */
-export const addColumn = <T extends QueryBuilder>(
+export const addColumn = <
+	T extends
+		| CreateTableBuilder<string, never>
+		| AlterTableColumnAlteringBuilder
+		| AlterTableBuilder,
+>(
 	query: T,
 	operation: AddColumnOperation,
 	db: DatabaseAdapter,
@@ -66,18 +68,65 @@ export const addColumn = <T extends QueryBuilder>(
 	) as T;
 };
 
-export const dropColumn = <T extends QueryBuilder>(
+/**
+ * Drops a column from a table using the provided query builder
+ */
+export const dropColumn = <
+	T extends AlterTableColumnAlteringBuilder | AlterTableBuilder,
+>(
 	query: T,
 	operation: RemoveColumnOperation,
 	db: DatabaseAdapter,
 ): T => {
-	return query as T;
+	return query.dropColumn(operation.columnName) as T;
 };
 
-export const modifyColumn = <T extends QueryBuilder>(
+/**
+ * Modifies an existing column in a table using the provided query builder
+ * @todo need to pass down column type as we likely need to use the modifyColumn method which requires it for its second arg
+ */
+export const modifyColumn = <
+	T extends AlterTableColumnAlteringBuilder | AlterTableBuilder,
+>(
 	query: T,
 	operation: ModifyColumnOperation,
 	db: DatabaseAdapter,
 ): T => {
-	return query as T;
+	const { columnName, changes } = operation;
+	let modifiedQuery = query;
+
+	//* handles type, nullable and default changes
+	if (
+		changes.type ||
+		changes.nullable !== undefined ||
+		changes.default !== undefined
+	) {
+		modifiedQuery = modifiedQuery.alterColumn(columnName, (col) => {
+			if (changes.type) {
+				col.setDataType(changes.type.to);
+			}
+
+			if (changes.nullable !== undefined) {
+				if (changes.nullable.to) col.dropNotNull();
+				else col.setNotNull();
+			}
+
+			if (changes.default !== undefined) {
+				if (changes.default.to === undefined) col.dropDefault();
+				else col.setDefault(db.formatDefaultValue(changes.default.to));
+			}
+
+			return col as unknown as AlteredColumnBuilder;
+		}) as T;
+	}
+
+	//* handle unique
+	if (changes.unique) {
+	}
+
+	//*andle foreign key changes
+	if (changes.foreignKey) {
+	}
+
+	return modifiedQuery;
 };
