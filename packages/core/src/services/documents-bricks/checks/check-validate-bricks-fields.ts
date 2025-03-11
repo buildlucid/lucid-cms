decdc1a5d7069c9d1eef4535cf0dd0344cbecbbd
William Yallop <hello@williamyallop.com>
2025-03-11 20:11:05 +0000

    Scaffolding brick/field validation

--- packages/core/src/services/documents-bricks/checks/check-validate-bricks-fields.ts
+++ packages/core/src/services/documents-bricks/checks/check-validate-bricks-fields.ts
@@ -1,7 +1,7 @@
 import T from "../../../translations/index.js";
 import logger from "../../../utils/logging/index.js";
 import Repository from "../../../libs/repositories/index.js";
-import type { FieldErrors } from "../../../types/errors.js";
+// import type { FieldErrors } from "../../../types/errors.js";
 import type { FieldTypes } from "../../../libs/custom-fields/types.js";
 import type BrickBuilder from "../../../libs/builders/brick-builder/index.js";
 import type CollectionBuilder from "../../../libs/builders/collection-builder/index.js";
@@ -11,6 +11,17 @@ import type {
 } from "../../../utils/services/types.js";
 import type { BrickSchema } from "../../../schemas/collection-bricks.js";
 import type { FieldSchemaType } from "../../../types.js";
+
+export interface FieldErrors {
+	key: string;
+	localeCode: string;
+	message: string;
+	groupErrors?: Array<Array<FieldErrors>>;
+}
+
+interface BrickError {
+	fields: FieldErrors[];
+}
 
 const checkValidateBricksFields: ServiceFn<
 	[
@@ -22,10 +33,46 @@ const checkValidateBricksFields: ServiceFn<
 	],
 	undefined
 > = async (context, data) => {
+	/*
+        1. work out and extract all media id's and user id's    
+        2. fetch data required for validation 
+            - users
+            - media
+        3. validate bricks
+            - loop through them all and call a recursiveFieldValidate fn
+            - return any/all of the errors
+        4. validate fields
+            - call recursiveFieldValidate fn
+            - return any/all of the errors
+        5. if either field or brick errors are > than 0 reurn an error object - this will parody the brick/field nested structure instead of being flat like before
+        6. return success
+    */
+
+	const brickErrors = validateBricks(data.bricks, data.collection);
+	const validateFields = recursiveFieldValidate(data.fields, data.collection);
+
 	return {
 		data: undefined,
 		error: undefined,
 	};
+};
+
+const validateBricks = (
+	brick: Array<BrickSchema>,
+	collection: CollectionBuilder,
+): Array<BrickError> => {
+	const errors: BrickError[] = [];
+
+	return errors;
+};
+
+const recursiveFieldValidate = (
+	fields: Array<FieldSchemaType>,
+	collection: CollectionBuilder,
+) => {
+	const errors: FieldErrors[] = [];
+
+	return errors;
 };
 
 export default checkValidateBricksFields;
