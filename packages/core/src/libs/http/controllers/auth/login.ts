6966c2dcea0aaa36392ab2f18849f4283bdec8d5
William Yallop <hello@williamyallop.com>
2025-06-01 19:52:05 +0100

    z validator copied in and updated to zod v4 and made a wrapper for it to throw errors in the same way as our fastify validation middleware

--- packages/core/src/libs/http/controllers/auth/login.ts
+++ packages/core/src/libs/http/controllers/auth/login.ts
@@ -1,9 +1,18 @@
 import { createFactory } from "hono/factory";
+import validate from "../../middleware/validate.js";
+import { controllerSchemas } from "../../../../schemas/auth.js";
 
 const factory = createFactory();
 
-const loginController = factory.createHandlers(async (c) => {
-	return c.json({ message: "Hello, world!" });
-});
+const loginController = factory.createHandlers(
+	validate("json", controllerSchemas.login.body),
+	async (c) => {
+		const { usernameOrEmail, password } = c.req.valid("json");
+
+		console.log(usernameOrEmail, password);
+
+		return c.json({ message: "Hello, world!" });
+	},
+);
 
 export default loginController;
