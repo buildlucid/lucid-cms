d9077bd27c54707dbbb9c626f5ee9d58fc4eb64f
William Yallop <hello@williamyallop.com>
2025-06-01 19:56:09 +0100

    onError callback added

--- packages/core/src/libs/http/app.ts
+++ packages/core/src/libs/http/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import routes from "./routes/v1/index.js";
+import { decodeError } from "../../utils/errors/index.js";
 import type { Config } from "../../types.js";
 import type { LucidHonoContext } from "../../types/hono.js";
 
@@ -14,7 +15,20 @@ const createApp = async (props: {
 			c.set("config", props.config);
 			await next();
 		})
-		.route("/", routes);
+		.route("/", routes)
+		.onError(async (err, c) => {
+			const { name, message, status, errorResponse, code } = decodeError(err);
+
+			// Todo: add logging
+
+			return c.json({
+				status,
+				code,
+				name,
+				message,
+				errors: errorResponse,
+			});
+		});
 
 	return app;
 };
