d07a207c8500926e883d2401759aa85798aa47dc
William Yallop <hello@williamyallop.com>
2025-06-21 20:08:05 +0100

    Replaced pino due to not working in cloudflare workers, needs revisiting and integrating with hono/logger and config options

--- packages/core/src/libs/config/lucid-config.ts
+++ packages/core/src/libs/config/lucid-config.ts
@@ -9,7 +9,7 @@ import BrickConfigSchema from "../builders/brick-builder/schema.js";
 import { LucidError } from "../../utils/errors/index.js";
 import CustomFieldSchema from "../custom-fields/schema.js";
 import logger from "../../utils/logging/index.js";
-import { initialiseLogger } from "../../utils/logging/logger.js";
+// import { initialiseLogger } from "../../utils/logging/logger.js";
 import constants from "../../constants/constants.js";
 import inferSchema from "../../services/collection-migrator/schema/infer-schema.js";
 import type { Config, LucidConfig } from "../../types/config.js";
@@ -90,7 +90,8 @@ const lucidConfig = async (config: LucidConfig) => {
 			collection.collectionTableSchema = res.data;
 		}
 
-		initialiseLogger(configRes.logTransport, configRes.logLevel);
+		// TODO: add back support
+		// initialiseLogger(configRes.logTransport, configRes.logLevel);
 
 		return configRes;
 	} catch (err) {
--- packages/core/src/utils/logging/logger.ts
+++ packages/core/src/utils/logging/logger.ts
@@ -1,34 +1,21 @@
-import { pino } from "pino";
-import type { DestinationStream } from "pino";
-
-let activePinoLogger = pino({
-	level: "info",
-});
-
-/**
- * Initialise the pino logger with optional custom transport
- */
-export const initialiseLogger = (
-	transport?: DestinationStream,
-	level?: "info" | "debug" | "warn" | "error",
-) => {
-	if (transport) {
-		activePinoLogger = pino(
-			{
-				level: level || "info",
-			},
-			transport,
-		);
-	} else {
-		if (level) {
-			activePinoLogger.level = level;
-		}
-	}
-};
+// TODO: integrate with hono logger and config options, this is just a placeholder so we have somthing that works with cloudflare workers
 
 /**
  * Get the current active pino logger instance
  */
-const getLogger = () => activePinoLogger;
+const getLogger = () => ({
+	error: (data: Record<string, unknown>, message: string) => {
+		console.error(JSON.stringify(data), message);
+	},
+	warn: (data: Record<string, unknown>, message: string) => {
+		console.warn(JSON.stringify(data), message);
+	},
+	info: (data: Record<string, unknown>, message: string) => {
+		console.info(JSON.stringify(data), message);
+	},
+	debug: (data: Record<string, unknown>, message: string) => {
+		console.debug(JSON.stringify(data), message);
+	},
+});
 
 export default getLogger;
